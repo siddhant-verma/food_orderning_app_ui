bf2c02419ee73defee0bf74c75cc34d5de4b413d
siddhant-verma <siddhantverma23.sv@gmail.com>
2023-02-02 17:25:38 +0530

    first commit

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,42 @@
+import 'package:deli_meals/screens/category_meals_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import '../screens/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+  CategoryItem({required this.id, required this.title, required this.color});
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .pushNamed('/category-meals', arguments: {'id': id, 'title': title});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.5), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
